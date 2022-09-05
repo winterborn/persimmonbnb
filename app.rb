42ba6d88ee4c9a22f18cd93ad1a555b27f4e0a13
Philip White <phwhite89@gmail.com>
2022-09-05 15:36:26 +0100

    clean up.

--- app.rb
+++ app.rb
@@ -1,19 +1,10 @@
 # file: app.rb
 
 require_relative "lib/database_connection"
-
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect("music_library")
-
-# Perform a SQL query on the database and get the result set.
-sql = "SELECT id, title FROM albums;"
-result = DatabaseConnection.exec_params(sql, [])
-
-# Print out each record from the result set .
-result.each { |record| p record }
-
 require "sinatra/base"
 require "sinatra/reloader"
+
+DatabaseConnection.connect("bnb_test")
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
