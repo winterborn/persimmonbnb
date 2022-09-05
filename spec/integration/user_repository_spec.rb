0dcf5594c6ed846af83dd5e4049ab1f8a89aca34
Philip White <phwhite89@gmail.com>
2022-09-05 17:53:58 +0100

    updated files to correct directory.

--- spec/integration/user_repository_spec.rb
+++ spec/integration/user_repository_spec.rb
@@ -1,40 +1,35 @@
-require 'user_repository'
+require "user_repository"
 
 RSpec.describe UserRepository do
+  def reset_users_table
+    seed_sql = File.read("spec/test_seeds/seeds_users.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "bnb_test" })
+    connection.exec(seed_sql)
+  end
 
-    def reset_users_table
-        seed_sql = File.read('spec/seeds_users.sql')
-        connection = PG.connect({ host: '127.0.0.1', dbname: 'bnb_test' })
-        connection.exec(seed_sql)
+  describe "#all" do
+    before(:each) { reset_users_table }
+
+    it "returns an array of all user details" do
+      repo = UserRepository.new
+
+      user = repo.all
+      expect(user[0].email).to eq "makers@hotmail.com"
+      expect(user[0].username).to eq "makers1"
     end
+  end
 
-    describe "#all" do
+  it "Creates an account" do
+    repo = UserRepository.new
+    new_user = User.new
 
-    before(:each) do 
-        reset_users_table
-        end
+    new_user.email = "makers2@hotmail.com"
+    new_user.username = "Toby814"
 
-        it "returns an array of all user details" do
-            repo = UserRepository.new
+    repo.create(new_user)
+    results = repo.all
 
-            user = repo.all
-            expect(user[0].email).to eq 'makers@hotmail.com'
-            expect(user[0].username).to eq 'makers1'
-        end
-    end
-
-    it "Creates an account" do
-        repo = UserRepository.new
-        new_user = User.new
-
-        new_user.email = 'makers2@hotmail.com'
-        new_user.username = 'Toby814'
-
-        repo.create(new_user)
-        results = repo.all
-
-        expect(results[1].email).to eq 'makers2@hotmail.com'
-        expect(results[1].username).to eq 'Toby814'
-    end 
-
-end 
+    expect(results[1].email).to eq "makers2@hotmail.com"
+    expect(results[1].username).to eq "Toby814"
+  end
+end
--- spec/test_seeds/seeds_spaces.sql
+++ spec/test_seeds/seeds_spaces.sql
@@ -2,7 +2,7 @@ TRUNCATE TABLE users RESTART IDENTITY CASCADE;
 TRUNCATE TABLE listed_spaces RESTART IDENTITY;
 
 INSERT INTO users (email, username) values ('makers@hotmail.com', 'makers1');
-INSERT INTO users (email, username) values ('hotstuff@gmail.com', 'hotstuff');
+INSERT INTO users (email, username) values ('makers2@hotmail.com', 'Toby814');
 INSERT INTO listed_spaces (space_name, space_description, space_price, available_dates, booked, user_id) VALUES ('Cottage', 'A lovely, three bedroom cottage.', 100, '2022-09-05', false, 1);
 INSERT INTO listed_spaces (space_name, space_description, space_price, available_dates, booked, user_id) VALUES ('Apartment', 'A high-rise apartment in central London.', 200, '2022-09-05', true, 2);
 INSERT INTO listed_spaces (space_name, space_description, space_price, available_dates, booked, user_id) VALUES ('House', 'A house in leafy Richmond.', 600, '2022-09-05', false, 1);
