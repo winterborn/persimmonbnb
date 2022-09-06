440d3503adf63bb5b9cdf8969c64c49afa6f3fa6
Britanni <banglestone@yahoo.co.uk>
2022-09-06 15:52:20 +0100

    created post route for newspace and implemented form.

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -26,4 +26,31 @@ describe Application do
                 expect(response.body).to include ('<h1>Book a Space</h1>')
             end
         end
+
+        context "GET /newspace" do
+            it "should return a new space page" do
+            response = get("/newspace")
+            expect(response.status).to eq 200
+            expect(response.body).to include "<h1>List a new Space:</h1>"
+            end
+        end
+  
+
+        context "POST /newspace" do
+            it "creates a new space" do
+            response = post("/spaces",
+                space_name: "Shed",
+                space_description: "A spacious shed.",
+                space_price: 1000,
+                start_date: "2022-10-01",
+                end_date: "2022-10-31",
+                user_id: 1
+                )
+            expect(response.status).to eq 200
+            expect(
+                response.body
+            ).to include "<h1>Book a Space</h1>"
+
+            end
+        end
     end
